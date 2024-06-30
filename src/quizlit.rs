6f03c003159a0bf4beb44be0073a0f6b9cc154e4
Marcus Willock <crazcalm@gmail.com>
2024-06-30 18:52:34 -0400

    fix #4 Parse quizlit json to structs

--- src/quizlit.rs
+++ src/quizlit.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum QuestionType {
     TrueFalse,
     Selection,
@@ -16,6 +16,53 @@ impl QuestionType {
             x => Err(format!("\"{x}\" is not  a supported Question type")),
         }
     }
+}
+
+#[allow(dead_code)]
+pub trait QuestionTrait {
+    fn get_type(&self) -> &QuestionType;
+    fn get_question(&self) -> Result<String, String>;
+    fn get_answers(&self) -> Result<Answers, String>;
+}
+
+impl QuestionTrait for Question {
+    fn get_type(&self) -> &QuestionType {
+        &self.question_type
+    }
+    fn get_question(&self) -> Result<String, String> {
+        if self.data.get("question").is_none() {
+            return Err("Could not find the question field".to_string());
+        }
+
+        Ok(self
+            .data
+            .get("question")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_string())
+    }
+
+    fn get_answers(&self) -> Result<Answers, String> {
+        if self.data.get("answers").is_none() {
+            return Err("Could not find the answers field".to_string());
+        }
+
+        let data = self.data.get("answers").unwrap().clone();
+        let question_type = self.get_type().clone();
+
+        Ok(Answers {
+            data,
+            question_type,
+        })
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Answers {
+    data: serde_json::Value,
+    question_type: QuestionType,
 }
 
 #[derive(Debug)]
@@ -71,9 +118,14 @@ mod tests {
 
         let json_value = serde_json::from_str(raw_json).unwrap();
 
-        let question = Question::new(json_value);
+        let question = Question::new(json_value).unwrap();
 
-        assert!(question.is_ok());
+        assert_eq!(question.get_type(), &QuestionType::TrueFalse);
+        assert_eq!(
+            question.get_question().unwrap(),
+            "Is the number 9 is afraid of the number 7?".to_string()
+        );
+        assert!(question.get_answers().is_ok());
     }
 
     #[test]
